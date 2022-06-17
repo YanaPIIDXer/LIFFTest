f5942534a1432c93541c7934f23bd5de7c80f7a3
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-18 03:43:46 +0900

    暴言送信機能実装

--- liff-app/src/App.vue
+++ liff-app/src/App.vue
@@ -2,12 +2,14 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld :msg="message" />
+    <button @click="sendMessage">メッセージ送信</button>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import liff from '@line/liff'
+import axios from 'axios'
 
 export default {
   name: 'App',
@@ -41,6 +43,38 @@ export default {
       this.message = "LIFF GetProfile Error..."
     }
   },
+  methods: {
+    /**
+     * メッセージ送信
+     */
+    sendMessage: function () {
+      if (!liff.isLoggedIn()) {
+        alert('ログインしてください')
+        return
+      }
+
+      let userId = null
+      try {
+        const profile = await liff.getProfile()
+        userId = profile.userId
+      } catch {
+        alert('プロフィール取得失敗')
+        return
+      }
+
+      try {
+        const response = await axios.post('https://yanap-liff-app-bot.herokuapp.com/message', {
+          userId: userId,
+          message: 'Motherfucking, MOTHERFUCKER!!',
+        })
+        if (!response || !response.data.result) { throw response }
+      } catch {
+        alert('メッセージ送信失敗')
+        return
+      }
+      alert('メッセージを送信しました')
+    },
+  }
 }
 </script>
 
