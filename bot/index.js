141fcc5985b2902295ac1b0957605c73095af31b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-18 03:51:20 +0900

    heroku向けのCORS対応

--- bot/index.js
+++ bot/index.js
@@ -7,22 +7,8 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 
-const allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization, access_token'
-    )
-
-    // intercept OPTIONS method
-    if ('OPTIONS' === req.method) {
-        res.send(200)
-    } else {
-        next()
-    }
-}
-app.use(allowCrossDomain)
+const cors = requrie('cors')
+app.use(cors())
 
 const line = require('@line/bot-sdk')
 const lineConfig = {
