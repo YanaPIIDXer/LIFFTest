8a9b742b06daba34f69f2d90244b1f18d7b49c0a
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-18 03:47:33 +0900

    CORS対応

--- bot/index.js
+++ bot/index.js
@@ -7,6 +7,23 @@ app.use(bodyParser.urlencoded({
 }))
 app.use(bodyParser.json())
 
+const allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE')
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Content-Type, Authorization, access_token'
+    )
+
+    // intercept OPTIONS method
+    if ('OPTIONS' === req.method) {
+        res.send(200)
+    } else {
+        next()
+    }
+}
+app.use(allowCrossDomain)
+
 const line = require('@line/bot-sdk')
 const lineConfig = {
     channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
