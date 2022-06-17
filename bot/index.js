7f706d90971a8cd06ac164c056483cae02d304f2
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-18 02:50:09 +0900

    とりあえず何か言ったら暴言を返すBOT

--- bot/index.js
+++ bot/index.js
@@ -1,8 +1,30 @@
 const express = require('express')
 const app = express()
 
+const line = require('@line/bot-sdk')
+const lineConfig = {
+    channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+    channelSecret: process.env.CHANNEL_SECRET,
+}
+const lineClient = new line.Client(lineConfig)
+
 app.get('/', (req, res) => {
     res.send('Hello, World.')
+})
+
+const lineCallback = (e) => {
+    return lineClient.replyMessage(e.replyToken, {
+        type: 'text',
+        text: 'Fuck.',
+    })
+}
+app.post('/callback', line.middleware(lineConfig), (req, res) => {
+    Promise.all(req.body.events.map(lineCallback))
+        .then(r => res.json(r))
+        .catch(err => {
+            console.error(err)
+            res.status(500).end()
+        })
 })
 
 const port = process.env.PORT || 3000
