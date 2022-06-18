13c965921e98b1b3e9ee1aa1f4030f2c1dff591e
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-18 16:09:04 +0900

    拡張子修正

--- bot/index.mjs
+++ bot/index.mjs
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 
-import postgresClient from './PostgresClient.js'
+import postgresClient from './PostgresClient.mjs'
 
 const cors = require('cors')
 app.use(cors())
