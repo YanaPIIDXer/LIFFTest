63de2a7b36ba08784a7a4a6af4994ca4d94cc8dd
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-18 13:26:32 +0900

    色々と撤去

--- liff-app/src/App.vue
+++ liff-app/src/App.vue
@@ -1,44 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld :msg="message" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import liff from '@line/liff'
+//import liff from '@line/liff'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   },
   data: function () {
     return {
-      message: 'Hello, World',
-    }
-  },
-  mounted: async function () {
-    try {
-      await liff.init({
-        liffId: process.env.VUE_APP_LIFF_ID,
-      })
-    } catch {
-      this.message = "LIFF Init Error..."
-      return
-    }
-    this.message = "LIFF Init OK!"
-
-    if (!liff.isLoggedIn()) {
-      liff.login()
-    }
-    
-    try {
-      const profile = await liff.getProfile()
-      this.message = profile.displayName
-    } catch {
-      this.message = "LIFF GetProfile Error..."
     }
   },
   methods: {
