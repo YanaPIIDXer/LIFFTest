456e511218f20ea1f4d173d8d169825644293ddb
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-18 17:35:22 +0900

    認証処理の実装

---
+++ bot/auth.mjs
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const verifyUrl = 'https://api.line.me/oauth2/v2.1/verify'
+
+export default async (token) => {
+    const params = new URLSearchParams()
+    params.append('id_token', token)
+    params.append('client_id', process.env.CLIENT_ID)
+
+    const response = await axios.post(verifyUrl, params, {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+        }
+    })
+    if (!response || response.status !== 200) { return null }
+    return {
+        id: response.data.sub,
+        name: response.data.name,
+    }
+}
--- bot/index.mjs
+++ bot/index.mjs
@@ -3,6 +3,7 @@ import bodyParser from 'body-parser'
 import cors from 'cors'
 import postgresClient from './PostgresClient.mjs'
 import line from '@line/bot-sdk'
+import verifyToken from './auth.mjs'
 
 const app = express()
 
@@ -63,7 +64,33 @@ app.get('/users', async (req, res) => {
 })
 
 app.post('/register', bodyParser.json(), async (req, res) => {
-    res.json({ result: true })
+    let result = false
+    let statusCode = 200
+
+    const token = req.body.token
+    // トークンの検証
+    const verify = await verifyToken(token)
+    if (!verify) {
+        res.status(400).json({ result: false })
+        return
+    }
+
+    const client = postgresClient()
+    try {
+        await client.connect()
+
+        // ユーザ登録がされていなければ新規登録
+        const results = await client.query('SELECT token from users where token = $1', [token])
+        if (!results.rows.length) {
+            await client.query('INSERT INTO users VALUES($1)', token)
+            result = true
+        }
+    } catch (error) {
+        console.error(error)
+        statusCode = 500
+    }
+    
+    res.status(statusCode).json({ result: result })
 })
 
 /*
