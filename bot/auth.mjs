601a2a237bb551af25265c9cbaac76f1d275aa1f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-18 18:07:43 +0900

    要求されているのがLIFFの方のチャネルIDっぽいので修正

--- bot/auth.mjs
+++ bot/auth.mjs
@@ -5,7 +5,7 @@ const verifyUrl = 'https://api.line.me/oauth2/v2.1/verify'
 export default async (token) => {
     const params = new URLSearchParams()
     params.append('id_token', token)
-    params.append('client_id', process.env.CLIENT_ID)
+    params.append('client_id', process.env.LIFF_CLIENT_ID)
 
     try {
         const response = await axios.post(verifyUrl, params, {
