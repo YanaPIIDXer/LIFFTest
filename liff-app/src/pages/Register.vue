03d072cb40748ef2404cc1bcf2a6f27a728b6ac8
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-18 17:05:19 +0900

    登録画面をとりあえずガワだけ作成

---
+++ liff-app/src/pages/Register.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="register">
+    <button @click="onRegister">ユーザ登録</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Register',
+  methods: {
+    /**
+     * 登録ボタン押下
+     */
+    onRegister: async function () {
+      alert('登録ボタン押下')
+    }
+  }
+}
+</script>
