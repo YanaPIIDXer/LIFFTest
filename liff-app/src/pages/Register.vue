0553a30378e9aceff35f154cac609321eae59a08
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-18 17:39:26 +0900

    登録処理を実装

--- liff-app/src/pages/Register.vue
+++ liff-app/src/pages/Register.vue
@@ -5,6 +5,9 @@
 </template>
 
 <script>
+import liff from '@line/liff'
+import axios from 'axios'
+
 export default {
   name: 'Register',
   methods: {
@@ -12,7 +15,29 @@ export default {
      * 登録ボタン押下
      */
     onRegister: async function () {
-      alert('登録ボタン押下')
+      if (!liff.isLoggedIn()) {
+        alert('ログインしていません')
+        return
+      }
+
+      let token = null
+      try {
+        token = liff.getIDToken()
+      } catch {
+        alert('トークン取得エラー')
+        return
+      }
+
+      try {
+        const response = await axios.post('https://yanap-liff-app-bot.herokuapp.com/register', {
+          token: token,
+        })
+        if (!response || response.status !== 200) { throw response }
+      } catch {
+        alert('登録エラー')
+        return
+      }
+      alert('登録しました')
     }
   }
 }
