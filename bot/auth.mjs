1485e4ad556ed7e28fbc3dde099e43faf340776f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-18 18:04:55 +0900

    こっちの方が正しかったようなので戻した

--- bot/auth.mjs
+++ bot/auth.mjs
@@ -3,11 +3,12 @@ import axios from 'axios'
 const verifyUrl = 'https://api.line.me/oauth2/v2.1/verify'
 
 export default async (token) => {
+    const params = new URLSearchParams()
+    params.append('id_token', token)
+    params.append('client_id', process.env.CLIENT_ID)
+
     try {
-        const response = await axios.post(verifyUrl, {
-            id_token: token,
-            client_id: process.env.CLIENT_ID,
-        }, {
+        const response = await axios.post(verifyUrl, params, {
             headers: {
                 'Content-Type': 'application/x-www-form-urlencoded',
             }
