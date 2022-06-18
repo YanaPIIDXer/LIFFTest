02776da6ce851d1a3942142960d672935371691f
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-18 16:04:40 +0900

    書き方を変更

--- bot/index.js
+++ bot/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
 
-import postgresClient from './PostgresClient'
+const postgresClient = require('./PostgresClient')
 
 const cors = require('cors')
 app.use(cors())
