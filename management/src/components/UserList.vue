f87feda1f31b845743e51b63a56344920577a001
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2022-06-18 14:15:01 +0900

    ユーザリストComponent

---
+++ management/src/components/UserList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="user-list">
+    <select>
+      <option v-for="item in list" :key="item.userId">
+        {{ item.displayName }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserList',
+  data: function () {
+    return {
+      list: [{ userId: "", displayName: "" }]
+    }
+  },
+  mounted: async function () {
+
+  },
+}
+</script>
